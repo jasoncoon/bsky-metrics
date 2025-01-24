12a64175e5b00afd93dd527f0c563fb15a6f277d
jasoncoon <3598755+jasoncoon@users.noreply.github.com>
2025-01-24 12:08:57 -0600

    Updates

--- app.js
+++ app.js
@@ -15,182 +15,101 @@ const imgBanner = document.getElementById("imgBanner");
 const inputUsername = document.getElementById("inputUsername");
 const link = document.getElementById("link");
 const loading = document.getElementById("loading");
-const spanStatus = document.getElementById('status');
-const divProgress = document.getElementById('progress');
-const tableBodyFollowers = document.getElementById('followersTableBody');
+const spanStatus = document.getElementById("status");
+const divProgress = document.getElementById("progress");
+const tableBodyFollowers = document.getElementById("followersTableBody");
 
 let displayName;
-let chartData;
 
 buttonGo.onclick = (e) => {
   const username = inputUsername.value;
   const url = new URL(window.location.href);
   url.searchParams.set("username", username);
-  // console.log({ url });
   window.location.href = url;
   return false;
 };
 
 async function getProfile(username) {
   try {
-    spanStatus.innerText = 'Getting profile...'
+    spanStatus.innerText = "Getting profile...";
     let response = await fetch(
       `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${username}`
     );
     const profile = await response.json();
-    // console.log({ profile, response });
-    return profile;  
+    return profile;
   } catch (error) {
     console.error("error getting profile: ", error);
   }
 }
 
 async function updateProfileDisplay(profile) {
-    displayName = profile.displayName || profile.handle;
-    if (profile?.avatar && !imgAvatar.src) {
-      imgAvatar.src = profile.avatar;
-    }
-    if (profile?.banner && !imgBanner.src) {
-      imgBanner.src = profile.banner;
-    }
-    divDescription.innerText = profile.description ?? '';
-    divDisplayName.innerText = displayName;
-    divHandle.innerText = profile.handle;
-    document.getElementById(
-      "divFollows"
-    ).innerText = `Following: ${profile.followsCount.toLocaleString()}`;
-    divFollowers.innerText = `Followers: ${profile.followersCount.toLocaleString()}`;
-    divPosts.innerText = `Posts: ${profile.postsCount.toLocaleString()}`;
-    divCreated.innerText = `Member since: ${new Date(
-      profile.createdAt
-    ).toLocaleString()}`;
-    link.href = `https://bsky.app/profile/${profile.handle}`;
-    link.innerText = `https://bsky.app/profile/${profile.handle}`;
-}
-
-async function getAllFollowers(username) {
-  loading.style.display = "block";
-  spanStatus.innerText = 'Getting followers...'
-  const allFollowers = [];
-  chartData = [];
-  let subject = undefined;
-  let cursor = undefined;
-
-  const valuesByDate = {};
-
-  do {
-    const page = await getFollowersPage(username, cursor);
-    allFollowers.push(...page.followers);
-    cursor = page.cursor;
-    for (const follower of page.followers) {
-      let date = new Date(follower.createdAt);
-      date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-      let value = valuesByDate[date.toISOString()] ?? 0;
-      value += 1;
-      valuesByDate[date.toISOString()] = value;
-    }
-    for (const dateString of Object.keys(valuesByDate)) {
-      chartData.push([new Date(dateString), valuesByDate[dateString]]);
-    }
-    // console.log({ chartData });
-    drawChart();
-  } while (cursor !== undefined);
-
-  // console.log({ subject, allFollowers });
-  loading.style.display = "none";
-
-  return allFollowers;
-}
-
-async function getFollowersPage(username, cursor) {
-  const getFollowersUrl = `https://public.api.bsky.app/xrpc/app.bsky.graph.getFollowers?actor=${username}&limit=100&cursor=${
-    cursor ?? ""
-  }`;
-  try {
-    // console.log(getFollowersUrl);
-    let response = await fetch(getFollowersUrl);
-    const json = await response.json();
-    // console.log({ json, response });
-    return json;
-  } catch (error) {
-    console.error("error getting followers page: ", error);
+  displayName = profile.displayName || profile.handle;
+  if (profile?.avatar && !imgAvatar.src) {
+    imgAvatar.src = profile.avatar;
   }
+  if (profile?.banner && !imgBanner.src) {
+    imgBanner.src = profile.banner;
+  }
+  divDescription.innerText = profile.description ?? "";
+  divDisplayName.innerText = displayName;
+  divHandle.innerText = profile.handle;
+  document.getElementById(
+    "divFollows"
+  ).innerText = `Following: ${profile.followsCount.toLocaleString()}`;
+  divFollowers.innerText = `Followers: ${profile.followersCount.toLocaleString()}`;
+  divPosts.innerText = `Posts: ${profile.postsCount.toLocaleString()}`;
+  divCreated.innerText = `Member since: ${new Date(
+    profile.createdAt
+  ).toLocaleString()}`;
+  link.href = `https://bsky.app/profile/${profile.handle}`;
+  link.innerText = `https://bsky.app/profile/${profile.handle}`;
 }
 
-function drawChart() {
+function drawChart(chartData) {
   var chart = new google.visualization.LineChart(divFollowersChart);
 
-  chartData.sort((a, b) => a[0] - b[0]);
-
   var data = google.visualization.arrayToDataTable([
-    ["Date", "Joined Bluesky"],
+    ["Date", "Followers"],
     ...chartData,
   ]);
 
   chart.draw(data, {
-    title: `When ${displayName}'s followers joined Bluesky`,
+    title: `${displayName}'s followers`,
     legend: "none",
   });
 }
 
 async function onLoad() {
-  // console.log({ location: window.location });
   const urlParams = new URLSearchParams(window.location.search);
   const username = urlParams.get("username");
   inputUsername.value = username;
   if (username) {
     const profile = await getProfile(username);
     await updateProfileDisplay(profile);
-    const allFollowers = await getAllFollowers(username);
-    await getFollowerProfiles(allFollowers);
+    await loadFollowersChart(profile);
   }
 }
 
-async function getFollowerProfiles(allFollowers) {
+async function loadFollowersChart(profile) {
   loading.style.display = "block";
-  const followerProfiles = [];
-  tableBodyFollowers.innerHTML = '';
+  spanStatus.innerText = "Getting follower count history...";
 
-  let i = 1;
+  const response = await fetch(
+    `https://social-metrics.evilgeniuslabs.org/query?social=Bluesky&handle=${profile.handle}`
+  );
 
-  let topFollower;
+  const body = await response.json();
 
-  for (const follower of allFollowers) {
-    divProgress.innerText = `Getting follower profile ${i} of ${allFollowers.length}`;
-    const profile = await getProfile(follower.handle);
-    followerProfiles.push(profile);
-    if (!topFollower) topFollower = profile;
-    if (profile.followersCount > topFollower.followersCount) topFollower = profile;
-    i++;
+  console.log({ response, body });
+
+  const chartData = [];
+
+  for (const item of body.Items) {
+    chartData.push([new Date(item.date), item.followers]);
   }
 
-  followerProfiles.sort((a, b) => b.followersCount - a.followersCount);
-
-  for (const profile of followerProfiles) {
-    const row = tableBodyFollowers.insertRow();
-    let cell;
-    
-    cell = row.insertCell();
-    cell.innerHTML = `<a href="https://bsky.app/profile/${profile.handle}">${profile.displayName || profile.handle}</a>`;
-    
-    cell = row.insertCell();
-    cell.innerHTML = profile.followersCount?.toLocaleString();
-    
-    cell = row.insertCell();
-    cell.innerHTML = profile.followsCount.toLocaleString();
-    
-    cell = row.insertCell();
-    cell.innerHTML = ((profile.followsCount ?? 0) / (profile.followersCount ?? 1)).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:0});
-    
-    cell = row.insertCell();
-    cell.innerHTML = profile.postsCount.toLocaleString();
-    
-    cell = row.insertCell();
-    cell.innerHTML = new Date(profile.createdAt).toLocaleString();
-  }
+  drawChart(chartData);
 
   loading.style.display = "none";
-  divProgress.innerHTML = '';
-
-  // console.log({followerProfiles});
+  divProgress.innerHTML = "";
 }
--- intrinsic-human-value-leaderboard.js
+++ intrinsic-human-value-leaderboard.js
@@ -1,31 +1,5 @@
-const urlParams = new URLSearchParams(window.location.search);
-let handles = urlParams.get("handles")?.split(";");
-
-console.log({ handles });
-
-if (!handles?.length) {
-  handles = [
-    "alexglow.bsky.social",
-    "alpenglow.bsky.social",
-    "architeuthisflux.bsky.social",
-    "athousandprojects.com",
-    "bhencke.bsky.social",
-    "blenster.bsky.social",
-    "chipperdoodles.bsky.social",
-    "clomads.bsky.social",
-    "desertember.bsky.social",
-    "eka.hn",
-    "evilgeniuslabs.org",
-    "geekmomprojects.com",
-    "guydupont.bsky.social",
-    "ishotjr.bsky.social",
-    "joeycastillo.bsky.social",
-    "lasermistress.bsky.social",
-    "leeborg.bsky.social",
-    "settinger.net",
-    "straithe.com",
-  ];
-}
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(onLoad);
 
 const divLoading = document.getElementById("loading");
 const divProgress = document.getElementById("progress");
@@ -35,7 +9,44 @@ const buttonSubmitHandles = document.getElementById("buttonSubmitHandles");
 
 buttonSubmitHandles.addEventListener("click", submitHandles);
 
-loadTable();
+let handles;
+
+let chart;
+
+async function onLoad() {
+  const urlParams = new URLSearchParams(window.location.search);
+  handles = urlParams.get("handles")?.split(";");
+
+  if (!handles?.length) {
+    handles = [
+      "alexglow.bsky.social",
+      "alpenglow.bsky.social",
+      "architeuthisflux.bsky.social",
+      "athousandprojects.com",
+      "bhencke.bsky.social",
+      "blenster.bsky.social",
+      "chipperdoodles.bsky.social",
+      "clomads.bsky.social",
+      "desertember.bsky.social",
+      "eka.hn",
+      "evilgeniuslabs.org",
+      "geekmomprojects.com",
+      "guydupont.bsky.social",
+      "ishotjr.bsky.social",
+      "joeycastillo.bsky.social",
+      "lasermistress.bsky.social",
+      "leeborg.bsky.social",
+      "settinger.net",
+      "straithe.com",
+    ];
+  }
+
+  chart = new google.visualization.LineChart(divFollowersChart);
+
+  await loadTable();
+
+  await loadChart();
+}
 
 async function loadTable() {
   divLoading.style.display = "block";
@@ -45,7 +56,7 @@ async function loadTable() {
   let i = 1;
 
   for (const handle of handles) {
-    divProgress.innerText = `Getting profile ${i} of ${handles.length}`;
+    divProgress.innerText = `Getting profile ${i} of ${handles.length}: ${handle}`;
     const profile = await getProfile(handle);
     profile.handle = handle;
     profiles.push(profile);
@@ -112,7 +123,7 @@ async function loadTable() {
 
 async function getProfile(username) {
   try {
-    let response = await fetch(
+    const response = await fetch(
       `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${username}`
     );
     const profile = await response.json();
@@ -138,4 +149,91 @@ function submitHandles() {
   window.location = `/bsky-metrics/intrinsic-human-value-leaderboard.htm?handles=${newHandles.join(
     ";"
   )}`;
+}
+
+async function loadChart() {
+  divLoading.style.display = "block";
+
+  let i = 1;
+
+  // need to build a two-dimensional array like this:
+  // const sampleData = [
+  //   // ["Date", "handle1", "handle2", "..."],
+  //   [new Date("2024-01-23"), 0, 1],
+  //   [new Date("2024-01-24"), 1, 2],
+  // ];
+
+  // will start by building a map of dates to handle follower count:
+  // const sampleDates = {
+  //   "2024-01-23": [
+  //     {
+  //       handle: "handle1",
+  //       followers: 0,
+  //     },
+  //     {
+  //       handle: "handle2",
+  //       followers: 1,
+  //     },
+  //   ],
+  // };
+
+  const dateItems = {};
+  const loadedHandles = [];
+
+  for (const handle of handles) {
+    divProgress.innerText = `Getting followers chart data for profile ${i} of ${handles.length}: ${handle}`;
+
+    const response = await fetch(
+      `https://social-metrics.evilgeniuslabs.org/query?social=Bluesky&handle=${handle}`
+    );
+
+    const body = await response.json();
+
+    const items = body.Items;
+
+    items.sort((a, b) => a.date - b.date);
+
+    for (const item of items) {
+      let dateItem = dateItems[item.date];
+      if (!dateItem) {
+        dateItem = [];
+        dateItems[item.date] = dateItem;
+      }
+
+      dateItem.push({ handle, followers: item.followers });
+    }
+
+    loadedHandles.push(handle);
+
+    // build an array out of the dateItems
+    const chartData = [["Date", ...loadedHandles]];
+
+    for (const date of Object.keys(dateItems)) {
+      const dateItem = dateItems[date];
+
+      chartData.push([date, ...dateItem.map((item) => item.followers)]);
+    }
+
+    console.log({ chartData });
+
+    drawChart(chartData);
+
+    i++;
+  }
+
+  divLoading.style.display = "none";
+  divProgress.innerHTML = "";
+}
+
+function drawChart(chartData) {
+  // const testData = [
+  //   [new Date("2024-01-23"), 0, 1],
+  //   [new Date("2024-01-24"), 1, 2],
+  // ];
+
+  var data = google.visualization.arrayToDataTable(chartData);
+
+  chart.draw(data, {
+    title: `Followers over time`,
+  });
 }
